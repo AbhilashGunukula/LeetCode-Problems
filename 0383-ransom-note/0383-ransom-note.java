1f2afe03d6ef19fec88f10e987d98964f38aa486
Abhilash Reddy <79795384+AbhilashGunukula@users.noreply.github.com>
2024-10-10 15:45:22 -0500

    Time: 11 ms (46.39%), Space: 45.1 MB (41.65%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -3,17 +3,22 @@ class Solution {
 
         HashMap<Character, Integer> memo = new HashMap<>();
 
-        for(int i=0; i<magazine.length(); i++){
-            memo.put(magazine.charAt(i), memo.get(magazine.charAt(i))==null?1:memo.get(magazine.charAt(i))+1);
+        // Populate the memo with characters from magazine
+        for (int i = 0; i < magazine.length(); i++) {
+            char ch = magazine.charAt(i);
+            memo.put(ch, memo.getOrDefault(ch, 0) + 1); // Increment the count or set to 1 if not present
         }
 
-        for(int i=0; i<ransomNote.length(); i++){
-            if(memo.get(ransomNote.charAt(i))== null || memo.get(ransomNote.charAt(i))== 0) return false;
-            else{
-                memo.put(ransomNote.charAt(i),memo.get(ransomNote.charAt(i))-1);
+        // Check each character in ransomNote against the memo
+        for (int i = 0; i < ransomNote.length(); i++) {
+            char ch = ransomNote.charAt(i);
+            // If character is not present or no more left to use, return false
+            if (!memo.containsKey(ch) || memo.get(ch) == 0) {
+                return false;
             }
+            // Decrement the count in memo
+            memo.put(ch, memo.get(ch) - 1);
         }
-
         return true;
         
     }
