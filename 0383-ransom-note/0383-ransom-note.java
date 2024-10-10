7c91a30ffc43ada9c9e4157ed2cc2847371f9c3e
Abhilash Reddy <79795384+AbhilashGunukula@users.noreply.github.com>
2024-10-10 15:47:08 -0500

    Time: 1 ms (99.28%), Space: 44.6 MB (69.78%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,23 +1,15 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
 
-        HashMap<Character, Integer> memo = new HashMap<>();
+		if (ransomNote.length() > magazine.length()) return false;
+        int[] alphabets_counter = new int[26];
+        
+        for (char c : magazine.toCharArray())
+            alphabets_counter[c-'a']++;
 
-        // Populate the memo with characters from magazine
-        for (int i = 0; i < magazine.length(); i++) {
-            char ch = magazine.charAt(i);
-            memo.put(ch, memo.getOrDefault(ch, 0) + 1); // Increment the count or set to 1 if not present
-        }
-
-        // Check each character in ransomNote against the memo
-        for (int i = 0; i < ransomNote.length(); i++) {
-            char ch = ransomNote.charAt(i);
-            // If character is not present or no more left to use, return false
-            if (!memo.containsKey(ch) || memo.get(ch) == 0) {
-                return false;
-            }
-            // Decrement the count in memo
-            memo.put(ch, memo.get(ch) - 1);
+        for (char c : ransomNote.toCharArray()){
+            if (alphabets_counter[c-'a'] == 0) return false;
+            alphabets_counter[c-'a']--;
         }
         return true;
         
