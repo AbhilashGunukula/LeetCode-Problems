54d6ad6c3444d12f4d991bf6e2599d6d871e5303
Abhilash Reddy <79795384+AbhilashGunukula@users.noreply.github.com>
2025-03-08 14:58:34 -0600

    Time: 4 ms (96.59%), Space: 46.2 MB (37.36%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -2,13 +2,13 @@ import java.util.*;
 
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        // Two-pointer approach
+
         int left = 0;
         int right = arr.length - 1;
 
-        // Shrink the window size to exactly k elements
+
         while (right - left > k-1) {
-            // Compare the distance from 'x' for both ends of the window
+
             if (Math.abs(arr[left] - x) > Math.abs(arr[right] - x)) {
                 left++;
             } else {
@@ -16,7 +16,7 @@ class Solution {
             }
         }
 
-        // Create the result list with the k closest elements
+
         List<Integer> result = new ArrayList<>();
         for (int i = left; i <= right; i++) {
             result.add(arr[i]);
