f88665a8fedbd48832d688baf2f20e35b533250f
Abhilash Reddy <79795384+AbhilashGunukula@users.noreply.github.com>
2025-03-28 17:39:21 -0500

    Time: 0 ms (100%), Space: 42.5 MB (26.94%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,27 +1,29 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        
-        int first=m-1, second=n-1, k=m+n-1;
 
-        while(first>=0 & second >=0){
-            if(nums1[first]>=nums2[second]){
-                nums1[k]=nums1[first];
-                first--;
+        int totalLength = m+n-1;
+         m = m-1; n=n-1;
+
+        while(m>=0 && n>=0){
+            if(nums2[n]>nums1[m]){
+                nums1[totalLength] = nums2[n];
+                n--;
             }else{
-                nums1[k]=nums2[second];
-                second--;
+                nums1[totalLength] = nums1[m];
+                m--;
             }
-            k--;
+            totalLength--;
         }
 
-        while(first>=0){
-            nums1[k]=nums1[first];
-            first--;k--;
-
+        while(m>=0){
+            nums1[totalLength] = nums1[m];
+            m--;
+            totalLength--;
         }
-        while(second>=0){
-            nums1[k]=nums2[second];
-            second--;k--;
+         while(n>=0){
+            nums1[totalLength] = nums2[n];
+            n--;
+            totalLength--;
         }
         
     }
