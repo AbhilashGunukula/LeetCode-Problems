1a9940ac21b7e91224ac6762fa1cd63cf72cc4bb
Abhilash Reddy <79795384+AbhilashGunukula@users.noreply.github.com>
2025-03-28 10:24:52 -0500

    Time: 10 ms (95.87%), Space: 49.9 MB (5.36%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,36 +1,35 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
 
-        HashMap<Integer, Integer> map = new HashMap<>();
+        HashMap<Integer, Integer> store = new HashMap<>();
 
-        for(int i =0; i<nums.length; i++){
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        for(int num : nums){
+            store.put(num, store.getOrDefault(num,0)+1);
         }
 
-        List<Integer>[] list = new ArrayList[nums.length + 1];
-        for(int i = 0; i<list.length; i++){
+        List<Integer>[] list = new ArrayList[nums.length+1];
+
+        for(int i=0; i<=nums.length; i++){
             list[i] = new ArrayList<>();
         }
 
-        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
-            int frequency = entry.getValue();
-            list[frequency].add(entry.getKey());
-        } 
-
-        int[] res = new int[k];
-        int idx = 0;
-        for(int i= list.length-1; i>=0; i--){
-            for(int num: list[i]){
-                res[idx++] = num;
-                if(idx == k){
-                    return res;
-                }
-            }
+        for(Map.Entry<Integer, Integer> entry : store.entrySet()){
+            int count = entry.getValue();
+            list[count].add(entry.getKey());
         }
 
-        return new int[0];
+        int [] res = new int[k];
+        int idx = 0;
+        for(int i=list.length-1; i>=0; i--){
+            for(int num : list[i]){
+                res[idx] = num;
+                idx++;
+                if(idx == k) return res;
+            }
 
+        }
 
+        return res;
         
     }
 }
