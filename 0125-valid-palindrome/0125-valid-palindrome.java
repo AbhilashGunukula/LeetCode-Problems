1599bdc9b9f64e59f3d1a02c1cecdd38b83acc1d
Abhilash Reddy <79795384+AbhilashGunukula@users.noreply.github.com>
2025-03-28 16:45:10 -0500

    Time: 2 ms (99.1%), Space: 42.9 MB (83.59%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,28 +1,24 @@
 class Solution {
     public boolean isPalindrome(String s) {
         int left = 0;
-        int right = s.length() - 1;
+        int right = s.length()-1;
 
-        while (left < right) {
-            // Skip non-alphanumeric characters from the left
-            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+        while(left<right){
+            while(left<right && !Character.isLetterOrDigit(s.charAt(left))){
                 left++;
             }
-            // Skip non-alphanumeric characters from the right
-            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+            while(left<right && !Character.isLetterOrDigit(s.charAt(right))){
                 right--;
             }
 
-            // Compare characters, ignoring case
-            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+            if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
                 return false;
             }
-
-            // Move towards the center
             left++;
             right--;
         }
 
         return true;
+        
     }
 }
