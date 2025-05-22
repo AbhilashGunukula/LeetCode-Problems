ba232e3a6bccc5b9f5251a5afa3dbffc04a98a44
Abhilash Reddy <79795384+AbhilashGunukula@users.noreply.github.com>
2025-05-22 11:06:49 -0500

    Time: 7 ms (48.69%), Space: 46 MB (17.67%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,56 @@
+class Solution {
+    public boolean canFinish(int N, int[][] prerequisites) {
+        
+
+        ArrayList<Integer>[] adj = new ArrayList[N];
+
+        for(int i =0; i<N; i++) adj[i] = new ArrayList<>();
+
+        for(int [] it : prerequisites){
+            int u = it[0];
+            int v = it[1];
+            adj[v].add(u);
+        }
+
+        int [] topo = topoSort(N, adj);
+
+        if(topo.length < N) return false;
+        
+        
+        return true;
+    }
+
+    private int[] topoSort(int V, ArrayList<Integer>[] adj){
+
+        int [] inDegree = new int[V];
+
+        for(int i =0; i<V; i++){
+            for(int it : adj[i]){
+                inDegree[it]++;
+            }
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for(int i =0; i<V; i++){
+            if(inDegree[i] == 0) q.add(i);
+        }
+
+        int[] ans = new int[V];
+        int idx = 0;
+
+        while(!q.isEmpty()){
+            int node = q.poll();
+            ans[idx++] = node;
+
+            for(int it: adj[node]){
+                inDegree[it]--;
+
+                if(inDegree[it] == 0) q.add(it);
+            }
+
+        }
+
+        return Arrays.copyOfRange(ans, 0, idx);
+    }
+}
