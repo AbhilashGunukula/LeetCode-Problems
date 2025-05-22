dc77518e2cb34d7d353817e220a76474025e9c92
Abhilash Reddy <79795384+AbhilashGunukula@users.noreply.github.com>
2025-05-22 11:09:34 -0500

    Time: 5 ms (78.96%), Space: 46 MB (63.65%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -0,0 +1,55 @@
+class Solution {
+    public int[] findOrder(int N, int[][] prerequisites) {
+        
+        ArrayList<Integer>[] adj = new ArrayList[N];
+
+        for(int i =0; i<N; i++) adj[i] = new ArrayList<>();
+
+        for(int [] it : prerequisites){
+            int u = it[0];
+            int v = it[1];
+            adj[v].add(u);
+        }
+
+        int [] topo = topoSort(N, adj);
+
+        if(topo.length < N) return new int[]{} ;
+        
+        
+        return topo;
+    }
+
+    private int[] topoSort(int V, ArrayList<Integer>[] adj){
+
+        int [] inDegree = new int[V];
+
+        for(int i =0; i<V; i++){
+            for(int it : adj[i]){
+                inDegree[it]++;
+            }
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for(int i =0; i<V; i++){
+            if(inDegree[i] == 0) q.add(i);
+        }
+
+        int[] ans = new int[V];
+        int idx = 0;
+
+        while(!q.isEmpty()){
+            int node = q.poll();
+            ans[idx++] = node;
+
+            for(int it: adj[node]){
+                inDegree[it]--;
+
+                if(inDegree[it] == 0) q.add(it);
+            }
+
+        }
+
+        return Arrays.copyOfRange(ans, 0, idx);
+    }
+}
